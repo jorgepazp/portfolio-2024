1c71ba9972389ccf1dd898e48c39b82c9424ce49
Jorge Paz <paz.jorge1@gmail.com>
2024-01-21 22:58:41 -0300

    avances

--- src/app/components/ThreeScene.tsx
+++ src/app/components/ThreeScene.tsx
@@ -3,9 +3,11 @@ import { Canvas, ThreeElements, Vector3, useFrame, useThree } from '@react-three
 import React, { useRef, useEffect, useState } from 'react';
 import * as THREE from 'three';
 import BottomPyramid from './three/TruncatedPyramid';
-import { Args, CameraControls, Edges, GradientTexture, OrbitControls, OrthographicCamera } from '@react-three/drei';
+import { Args, CameraControls, CameraShake, Caustics, Edges, GradientTexture, MeshTransmissionMaterial, OrbitControls, OrthographicCamera } from '@react-three/drei';
+import { EffectComposer, Bloom } from '@react-three/postprocessing'
+import { KernelSize } from 'postprocessing'
 
-function Box({position,rotation,color=0xFFC759,cubeArgs=[.5,.5,.5]}) {
+function Box({position,rotation,color=0xFFBB34,cubeArgs=[.5,.5,.5]}) {
   const { viewport } = useThree();
   const ref = useRef<THREE.Mesh>(null!)
   const material = useRef<THREE.Material>(null!)
@@ -30,7 +32,7 @@ function Box({position,rotation,color=0xFFC759,cubeArgs=[.5,.5,.5]}) {
       rotation={rotation}
       ref={ref}>
       <boxGeometry args={cubeArgs as Args<any>} />
-      <meshStandardMaterial color={color} />
+      <meshPhongMaterial color={color}  />
       {/* <meshNormalMaterial/> */}
     </mesh>
   )
@@ -54,10 +56,12 @@ const MainCone = (props: ThreeElements['mesh']) =>{
     // ref.current.position.y += 0.1 * direction;
   })
   return (
+      
     <mesh receiveShadow={true} castShadow={true} ref={ref} position={[0,1.5,0]}>
     {/* <meshNormalMaterial/> */}
     {/* <meshPhongMaterial color={0xBADA55} shininess={200}/> */}
-    <meshStandardMaterial roughness={0} transparent={true} color={0x6366f1} />
+    {/* <meshPhongMaterial reflectivity={10} specular={0x00ff00} color={0x6366f1} /> */}
+    <MeshTransmissionMaterial resolution={1024}  distortion={0.2} color={0xffffff} reflectivity={0} thickness={10} anisotropy={2} distortionScale={0} temporalDistortion={0} />
     <coneGeometry args={[.5,.7,4]}/>
   </mesh>
   )
@@ -65,7 +69,7 @@ const MainCone = (props: ThreeElements['mesh']) =>{
 
 const Thorus = ({position,scale=([1,1,1] as Vector3)}) =>{
   const ref = useRef<THREE.Mesh>(null!)
-  const {pointer,viewport} = useThree();
+  const {pointer,viewport,camera} = useThree();
   useFrame((state,delta)=>{
     ref.current.rotation.x +=delta
     ref.current.rotation.z +=delta
@@ -77,44 +81,61 @@ const Thorus = ({position,scale=([1,1,1] as Vector3)}) =>{
     <mesh
     ref={ref}
     scale={scale}
+    castShadow
+    receiveShadow
     position={position}>
     <torusKnotGeometry  args={[1,.4,1032,64,1,1]}/>
-    <meshNormalMaterial wireframe/>
+    {/* <MeshTransmissionMaterial resolution={1024} distortion={0}  color={0x6366f1} thickness={1} anisotropy={10} distortionScale={1} temporalDistortion={10} /> */}
+
+    <meshNormalMaterial />
   </mesh>
   );
 }
 
-const BasePlatform = () =>{
+const BasePlatform = ({color=0xffffff}) =>{
   return(
-    <mesh receiveShadow rotation={[0,Math.PI/4,0]} position={[0,-1.2,0]}>
-        <boxGeometry args={[7,.5,7]} />
-        <meshLambertMaterial color={0xa5b4fc}/>
+    <mesh receiveShadow rotation={[0,Math.PI/4,0]} position={[0,-1,0]}>
+        <boxGeometry args={[7,.3,7]} />
+        <meshLambertMaterial color={color}/>
       </mesh>
   );
 }
 
 const ThreeScene: React.FC = () => {
   const containerRef = useRef<HTMLCanvasElement>(null);
-  
+  const [windowSize, setWindowSize] = useState([
+    window.innerWidth,
+    window.innerHeight,
+  ]);
+
+  useEffect(() => {
+    const handleWindowResize = () => {
+      setWindowSize([window.innerWidth, window.innerHeight]);
+    };
+
+    window.addEventListener('resize', handleWindowResize);
+
+    return () => {
+      window.removeEventListener('resize', handleWindowResize);
+    };
+  }, []);
   return (
-    <Canvas  shadows className='absolute' orthographic camera={{ zoom: 100, position: [0, 45, 100] }} >
-      
-    <ambientLight color={0xffffff} intensity={2} />
-
-  
-
-    <pointLight position={[0, 20, 0]} color={0xffffff} castShadow receiveShadow decay={0} intensity={2} />
-    <directionalLight color={'blue'}/>
+    <Canvas  shadows className='absolute' orthographic camera={{ zoom: (windowSize[0]-70)/15, position: [0, 45, 100] }} >
+    <ambientLight color={0xFADECE} intensity={1} />
+    {/* <CameraShake yawFrequency={0.2} pitchFrequency={0.2} rollFrequency={0.2} /> */}
+    <pointLight  position={[30, 20, 15]} color={0xddddfe} castShadow receiveShadow decay={0} intensity={5} ></pointLight>
+    <directionalLight color={0x0000ff} intensity={5} />
+    <hemisphereLight args={[0xffffbb, 0x080820, 1]} />
     <MainCone/>
-    <BottomPyramid/>
+    <BottomPyramid position={[0,0,0]}/>
     <Box color={0x6366f1} cubeArgs={[.6,.6,.6]}  position={[3,3,0]} rotation={[0,Math.PI/4,0]} />
-    <Box color={0xFF7B9C} cubeArgs={[1,1,1]} position={[-3.5,3,-1]} rotation={[0,Math.PI/4,0]}/>
+    <Box color={0xFF2259} cubeArgs={[1,1,1]} position={[-3.5,3,-1]} rotation={[0,Math.PI/4,0]}/>
     {/* <Box color={0x06A77D} position={[-2.5,4,2]} rotation={[0,Math.PI/4,0]}/> */}
     <Box position={[0,2,2.5]} rotation={[0,Math.PI/4,0]}/>
-    <gridHelper material={new THREE.LineBasicMaterial({color:'white'})} args={[50,20]} rotation={[0,Math.PI/4,0]} position={[0,-1,0]}/>
-      <BasePlatform/>
-      <OrbitControls />
-      <CameraControls dollyToCursor makeDefault />
+    <gridHelper material={new THREE.LineBasicMaterial({color:0x334155})} args={[50,50]} rotation={[0,Math.PI/4,0]} position={[0,-1.15,2.1]}/>
+      <BasePlatform color={0x99f6e4}/>
+      <OrbitControls enableZoom={false}  enablePan={false} autoRotate autoRotateSpeed={.4} />
+      {/* <CameraControls dollyToCursor makeDefault /> */}
 
       {/* <mesh position={[0,2,0]}>
         <planeGeometry args={[1,1,10,10]}/>
--- src/app/components/three/TruncatedPyramid.tsx
+++ src/app/components/three/TruncatedPyramid.tsx
@@ -3,55 +3,13 @@ import { useEffect, useRef, useState } from "react"
 import * as THREE from "three";
 import gridVertex from '@/app/shaders/grid/vertex.glsl';
 import gridFragment from '@/app/shaders/grid/fragment.glsl';
-import { Edges, GradientTexture } from "@react-three/drei";
+import { Edges, GradientTexture, MeshTransmissionMaterial } from "@react-three/drei";
 
 const BottomPyramid = (props: ThreeElements['mesh']) =>{
   const ref = useRef<THREE.Mesh>(null!)
   const geometry = useRef<THREE.CylinderGeometry>(null!)
   const [hovered, hover] = useState(false)
-  const [clicked, click] = useState(false)
-//   useFrame((state, delta) => (ref.current.rotation.x += delta))
-useEffect(()=>{
-    const darkblue = new THREE.Color(0x0f172a);
-    const blue = new THREE.Color(0x6366f1);
-    const white = new THREE.Color(0xf8fafc);
-    const gray = new THREE.Color(0xeef2ff);
-    const black = new THREE.Color(0xd1d1d1);
-    const pink = new THREE.Color(0xe91e63);
-    const orange = new THREE.Color(0xFF9800);
-    const red = new THREE.Color(0xf44336);
-    const green = new THREE.Color(0xbada55);
-  const nonIndexedGeometry = geometry.current.toNonIndexed();
-    const positionAttribute = nonIndexedGeometry.getAttribute('position')
-    console.log(positionAttribute.count)
-    const mix = [red,red,orange,orange,pink,pink];
-    const _colors = [];
-    const newColor = new THREE.Color();
-    for (let i = 0;i<positionAttribute.count;i+=3) {
-      newColor.set(mix[i/3]);
-      _colors.push(newColor.r);
-      _colors.push(newColor.g);
-      _colors.push(newColor.b);
-    }
-    const colors = new THREE.Float32BufferAttribute(_colors,3);
-    geometry.current.setAttribute("aVertexColor",colors);
-    geometry.current.computeVertexNormals
-    // const nonIndexedGeometry = geometry.current.toNonIndexed();
-    // const positionAttribute = nonIndexedGeometry.getAttribute('position');
-    // const color = new THREE.Color();
-    // const colors = [];
-    // console.log(positionAttribute.count)
-    // for(let i = 0; i< positionAttribute.count; i+=3){
 
-    //     color.set(Math.random() * 0xffffff);
-    //     colors.push(color.r,color.g,color.b);
-    //     console.log(color)
-    //     colors.push(color.r,color.g,color.b);
-    //     colors.push(color.r,color.g,color.b);
-    // }
-    // nonIndexedGeometry.setAttribute('color', new THREE.Float32BufferAttribute(colors,3));
-    // geometry.current.setAttribute('color', new THREE.Float32BufferAttribute(colors,3))
-},[]);
   return (
     <mesh
       {...props}
@@ -63,21 +21,9 @@ useEffect(()=>{
       onPointerOver={(event) => hover(true)}
       onPointerOut={(event) => hover(false)}>
       <cylinderGeometry ref={geometry}  args={[.5,2 ,2,4]} />
-      {/* <meshLambertMaterial vertexColors={true}/> */}
-      {/* <meshStandardMaterial  vertexColors={true}  color={hovered ? 'hotpink' : 'orange'} /> */}
-      {/* <shaderMaterial  vertexShader={gridVertex} fragmentShader={gridFragment}/> */}
-      <meshStandardMaterial>
-      <Edges
-    scale={1.1}
-    threshold={15} // Display edges only when the angle between two faces exceeds this value (default=15 degrees)
-    color="white"
-  />
-      <GradientTexture
-      stops={[0,0.8, 1]} // As many stops as you want
-      colors={[ "#c7d2fe",'#e0e7ff','#eef2ff']} // Colors need to match the number of stops
-      size={1024} // Size is optional, default = 1024
-    />
-      </meshStandardMaterial>
+      {/* <meshLambertMaterial emissive={0xffffff} emissiveIntensity={.3} color={0x6366f1} /> */}
+     <meshNormalMaterial vertexColors/>
+
     </mesh>
   )
 }
